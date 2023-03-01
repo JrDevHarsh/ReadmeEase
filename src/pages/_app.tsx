a754384314bcf740e407d43fdda2169735f9591d
Harshdeep Singh <harsh.code97@gmail.com>
2023-03-01 10:06:29 +0530

    fix: Add fonts at global level

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
+  display: "swap",
+  preload: true,
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={poppins.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import { Poppins } from "next/font/google";
 import PageSEO from "@/components/PageSEO";
 import Header from "@/components/navbar/Header";
-
-const poppins = Poppins({
-  weight: ["400", "500", "600"],
-  display: "swap",
-  preload: true,
-  style: "normal",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
@@ -18,7 +9,7 @@ export default function Home() {
         description="Create beautiful and professional readme.md files effortlessly with ReadmeEase."
       />
       <Header />
-      <main className={poppins.className}>ReadmeEase</main>
+      <main>ReadmeEase</main>
     </>
   );
 }
