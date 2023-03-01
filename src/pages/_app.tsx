6a713685e922a30dee6d384f0d951a05b2838e97
Harshdeep Singh <harsh.code97@gmail.com>
2023-03-01 10:07:45 +0530

    fix: Initialize Header at root to prevent re-render

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/navbar/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
@@ -13,6 +14,7 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={poppins.className}>
+      <Header />
       <Component {...pageProps} />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import PageSEO from "@/components/PageSEO";
-import Header from "@/components/navbar/Header";
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
         title="ReadmeEase | Create README Files Effortlessly"
         description="Create beautiful and professional readme.md files effortlessly with ReadmeEase."
       />
-      <Header />
       <main>ReadmeEase</main>
     </>
   );
